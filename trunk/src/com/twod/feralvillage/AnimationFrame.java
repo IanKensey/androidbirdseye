88207b7665b5c18f9b6f28f0846b16d264a6343d
IrishKinnan@gmail.com <IrishKinnan@gmail.com@ea2e3117-0b94-964b-ea7e-73622501aa2c>
2014-08-07 05:30:46 +0000

    Newly Uploaded

---
+++ trunk/src/com/twod/feralvillage/AnimationFrame.java
@@ -0,0 +1,25 @@
+package com.twod.feralvillage;
+
+public class AnimationFrame extends AllocationGuard {
+    public Texture texture;
+    public float holdTime;
+    FixedSizeArray<CollisionVolume> attackVolumes;
+    FixedSizeArray<CollisionVolume> vulnerabilityVolumes;
+   
+    public AnimationFrame(Texture textureObject, float animationHoldTime) {
+        super();
+        texture = textureObject;
+        holdTime = animationHoldTime;
+    }
+   
+    public AnimationFrame(Texture textureObject, float animationHoldTime,
+            FixedSizeArray<CollisionVolume> attackVolumeList,
+            FixedSizeArray<CollisionVolume> vulnerabilityVolumeList) {
+        super();
+        texture = textureObject;
+        holdTime = animationHoldTime;
+        attackVolumes = attackVolumeList;
+        vulnerabilityVolumes = vulnerabilityVolumeList;
+    }
+}
+
