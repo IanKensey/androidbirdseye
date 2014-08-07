c5a0e3c6d8e88630530282064f6099cdab4cf6fc
IrishKinnan@gmail.com <IrishKinnan@gmail.com@ea2e3117-0b94-964b-ea7e-73622501aa2c>
2014-08-07 05:32:34 +0000

    Newly Uploaded

---
+++ trunk/src/com/twod/feralvillage/CameraBiasComponent.java
@@ -0,0 +1,22 @@
+package com.twod.feralvillage;
+
+public class CameraBiasComponent extends GameComponent {
+         public CameraBiasComponent() {
+        super();
+        setPhase(GameComponent.ComponentPhases.THINK.ordinal());
+    }
+    
+    @Override
+    public void reset() {
+        
+    }
+    
+    @Override
+    public void update(float timeDelta, BaseObject parent) {   
+        GameObject parentObject = (GameObject)parent;
+        CameraSystem camera = sSystemRegistry.cameraSystem;
+        if (camera != null) {
+                camera.addCameraBias(parentObject.getPosition());
+        }
+        }
+}
