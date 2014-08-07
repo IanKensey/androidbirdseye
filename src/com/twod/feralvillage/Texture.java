aa94f7ffbfd3519b4a4fd8114c0faf4c16741f3a
IrishKinnan <IrishKinnan@gmail.com>
2014-08-07 06:06:32 +0000

    Newly Uploaded

---
+++ src/com/twod/feralvillage/Texture.java
@@ -0,0 +1,22 @@
+package com.twod.feralvillage;
+
+public class Texture extends AllocationGuard {
+    public int resource;
+    public int name;
+    public int width;
+    public int height;
+    public boolean loaded;
+    
+    public Texture() {
+        super();
+        reset();
+    }
+    
+    public void reset() {
+        resource = -1;
+        name = -1;
+        width = 0;
+        height = 0;
+        loaded = false;
+    }
+}
