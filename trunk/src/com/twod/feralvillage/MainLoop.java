c3ba2bb0928186605f73d7d2dbea0ff5317c2eb5
IrishKinnan@gmail.com <IrishKinnan@gmail.com@ea2e3117-0b94-964b-ea7e-73622501aa2c>
2014-08-07 05:52:52 +0000

    Newly Uploaded

---
+++ trunk/src/com/twod/feralvillage/MainLoop.java
@@ -0,0 +1,22 @@
+package com.twod.feralvillage;
+
+public class MainLoop extends ObjectManager {
+
+    // Ensures that time updates before everything else.
+    public MainLoop() {
+        super();
+        mTimeSystem = new TimeSystem();
+        sSystemRegistry.timeSystem = mTimeSystem;
+        sSystemRegistry.registerForReset(mTimeSystem);
+    }
+
+    @Override
+    public void update(float timeDelta, BaseObject parent) {
+        mTimeSystem.update(timeDelta, parent);
+        final float newTimeDelta = mTimeSystem.getFrameDelta();  // The time system may warp time.
+        super.update(newTimeDelta, parent);
+    }
+
+    private TimeSystem mTimeSystem;
+}
+
