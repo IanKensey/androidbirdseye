6c44d16d9e493f6384376c5ce2c9a1fbf907997e
IrishKinnan@gmail.com <IrishKinnan@gmail.com@ea2e3117-0b94-964b-ea7e-73622501aa2c>
2014-08-07 05:56:16 +0000

    Newly Uploaded

---
+++ trunk/src/com/twod/feralvillage/PhasedObject.java
@@ -0,0 +1,21 @@
+package com.twod.feralvillage;
+
+public class PhasedObject extends BaseObject {
+
+    public int phase;   // This is public because the phased is accessed extremely often, so much
+                        // so that the function overhead of an getter is non-trivial.  
+
+    public PhasedObject() {
+        super();
+    }
+   
+    @Override
+    public void reset() {
+       
+    }
+
+    public void setPhase(int phaseValue) {
+        phase = phaseValue;
+    }
+}
+
