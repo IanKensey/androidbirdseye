8f43db46c216dced9c910b2bf1da59a0686fd95d
IrishKinnan <IrishKinnan@gmail.com>
2014-08-07 05:41:36 +0000

    Newly Uploaded

---
+++ src/com/twod/feralvillage/FrameRateWatcherComponent.java
@@ -0,0 +1,34 @@
+package com.twod.feralvillage;
+
+public class FrameRateWatcherComponent extends GameComponent {
+        private RenderComponent mRenderComponent;
+        private DrawableObject mDrawable;
+        private float mMaxFrameTime = 1.0f / 30.0f;
+        
+        public FrameRateWatcherComponent() {
+        super();
+        setPhase(GameComponent.ComponentPhases.THINK.ordinal());
+    }
+    
+    @Override
+    public void reset() {
+        mRenderComponent = null;
+        mDrawable = null;
+    }
+    
+    @Override
+    public void update(float timeDelta, BaseObject parent) {   
+        if (mRenderComponent != null && mDrawable != null) {
+                if (timeDelta > mMaxFrameTime) {
+                        mRenderComponent.setDrawable(mDrawable);
+                } else {
+                        mRenderComponent.setDrawable(null);
+                }
+        }
+        }
+    
+    public void setup(RenderComponent render, DrawableObject drawable) {
+        mRenderComponent = render;
+        mDrawable = drawable;
+    }
+}
